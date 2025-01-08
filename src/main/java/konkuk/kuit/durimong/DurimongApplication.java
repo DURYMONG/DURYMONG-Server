65f5bda7867c5625da7fca3a271a3afbb12f6b08
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-01-08 12:51:56 +0900

    init: 프로젝트 생성

---
+++ src/main/java/konkuk/kuit/durimong/DurimongApplication.java
@@ -0,0 +1,13 @@
+package konkuk.kuit.durimong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurimongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DurimongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konkuk/kuit/durimong/DurimongApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.kuit.durimong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurimongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
