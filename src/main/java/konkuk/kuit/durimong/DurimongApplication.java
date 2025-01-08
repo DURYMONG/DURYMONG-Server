57ebd4acbe0da345f16ac74ea3bce920190f132f
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-01-08 13:10:08 +0900

    test

--- src/main/java/konkuk/kuit/durimong/DurimongApplication.java
+++ src/main/java/konkuk/kuit/durimong/DurimongApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DurimongApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DurimongApplication started");
 		SpringApplication.run(DurimongApplication.class, args);
 	}
 
