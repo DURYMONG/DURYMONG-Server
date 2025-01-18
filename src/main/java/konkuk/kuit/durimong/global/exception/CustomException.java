e7c121dc41ad3b51c20a6dace7491df97d359cb9
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-01-18 14:44:42 +0900

    feat: 전역적 예외처리 구현

---
+++ src/main/java/konkuk/kuit/durimong/global/exception/CustomException.java
@@ -0,0 +1,30 @@
+package konkuk.kuit.durimong.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private Exception originException;
+    private final ErrorCode errorCode;
+    private final String message;
+    private final String additionalInfo;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.additionalInfo = null;
+    }
+
+    public CustomException(ErrorCode errorCode, String additionalMessage) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.additionalInfo = additionalMessage;
+    }
+
+    public CustomException(ErrorCode errorCode, Exception exception) {
+        this.errorCode = errorCode;
+        this.originException = exception;
+        this.message = errorCode.getMessage();
+        this.additionalInfo = null;
+    }
+}
---
+++ src/main/java/konkuk/kuit/durimong/global/response/result/ParameterData.java
@@ -0,0 +1,16 @@
+package konkuk.kuit.durimong.global.response.result;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class ParameterData {
+    private String key;
+    private String value;
+    private String reason;
+}
