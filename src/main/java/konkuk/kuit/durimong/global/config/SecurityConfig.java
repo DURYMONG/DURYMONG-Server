f36f9a9a06d270a736937540d9a2e8f764af30c9
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-02-11 00:06:04 +0900

    feat: @EnableWebSecurity 어노테이션 추가

--- src/main/java/konkuk/kuit/durimong/global/config/SecurityConfig.java
+++ src/main/java/konkuk/kuit/durimong/global/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +25,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Slf4j
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
