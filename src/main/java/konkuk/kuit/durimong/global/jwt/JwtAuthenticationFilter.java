a2ca890a6b58be41b7242e4c3924a58cb7f5c8be
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-02-18 11:40:28 +0900

    feat: 로그아웃 시 Redis에 블랙리스트 처리

--- src/main/java/konkuk/kuit/durimong/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/konkuk/kuit/durimong/global/jwt/JwtAuthenticationFilter.java
@@ -4,13 +4,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import konkuk.kuit.durimong.global.exception.CustomException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -22,19 +25,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
 
-        // Swagger UI 경로를 제외한 요청에서만 인증을 적용
         if (shouldNotFilter(request)) {
             filterChain.doFilter(request, response);
             return;
         }
+
         String token = getToken(request);
-        if (token != null && jwtProvider.verify(request,token)) {
-            Authentication authentication = getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token != null) {
+            try {
+                if (jwtProvider.verify(request, token)) {
+                    Authentication authentication = getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (CustomException e) {
+                log.debug("토큰 검증 실패: {}", e.getMessage());
+                // 예외 발생 시 SecurityContextHolder에 인증정보를 저장하지 않음
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
     }
+
+
     /**
      * Swagger UI 또는 Swagger 관련 경로를 제외한 요청에 대해서만 필터를 적용
      */
